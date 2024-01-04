6c785332eb92c95cb15f32f75321ecf90a884f6c
gemtunde <gemtunde@gmail.com>
2024-01-04 17:32:41 +0100

    test about us 2

--- components/About/About.test.jsx
+++ components/About/About.test.jsx
@@ -1,9 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import About from "./index";
+import { it } from "node:test";
 
 describe("About Page", () => {
   it("should render about heading", () => {
     render(<About />);
     screen.getByRole("heading", { name: /About us/i });
+  });
+  it("should render acessibble search box", () => {
+    render(<About />);
+    const searchInput = screen.getByRole("searchbox");
+    expect(searchInput).toHaveAccessibleName("Search");
   });
 });
--- components/About/index.js
+++ components/About/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import commerce from "../../lib/commerce";
 
-const About = () => {
+export async function getStaticProps(context) {
+  const { data: products } = await commerce.products.list();
+  const { data: categories } = await commerce.categories.list();
+
+  return {
+    props: { products, categories },
+    revalidate: 30,
+  };
+}
+
+const About = ({ products, categories }) => {
+  console.log(products, categories);
+  //commerce.products.list().then((product) => console.log(product));
   return (
     <div>
       <h2>About Us</h2>
+      <input
+        role="searchbox"
+        type="text"
+        title="Search"
+        placeholder="Search products"
+      />
     </div>
   );
 };
